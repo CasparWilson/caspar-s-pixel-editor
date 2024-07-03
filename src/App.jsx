674608a25572d66005cebec499a19c20aef1f5cc
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-03 17:04:27 +0100

    grid and palette displayed, no functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-function App() {
+import {palette} from "./components/palette"
+
+function App(props) {
+
+    const pixels=[]
+    for(let i=0; i < props.gridLength**2; i++){
+        pixels.push(<button key={i} className="pixel"></button>)
+    }
+
+    const paletteButtons=palette.map((colour)=> {
+    return (<button key={colour} 
+        style={{backgroundColor: `#${colour}`}}/>
+    )})
+
+    console.log(palette, paletteButtons)
+
     return (
         <div>
             <h1>Pixel Editor</h1>
+            <div className="pixel-grid">
+                {pixels}
+            </div>
             
+            <h3>palette</h3>
+            <div className='paletteDisplay'>
+                {paletteButtons}
+            </div>
         </div>
+        
     );
 }
 
