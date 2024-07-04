991922a66250ac0f43dce9b73755287a82a396a6
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-04 11:30:48 +0100

    functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import {palette} from "./components/palette"
+import React from 'react'
 
 function App(props) {
 
+    
+    const pixelStateObject = {}
+    for(let i=0; i < props.gridLength**2; i++){
+        pixelStateObject[i]='#FFFFFF'
+    }
+
+    const[editorState, setEditorState] = React.useState(
+        {colourSelected: '#FFFFFF', ...pixelStateObject}
+    )
+
     const pixels=[]
     for(let i=0; i < props.gridLength**2; i++){
-        pixels.push(<button key={i} className="pixel"></button>)
-    }
+        pixels.push(<button key={i} className="pixel"
+            style={{backgroundColor: editorState[i]}}
+            onClick={()=> setEditorState((prevState) => {
+                console.log(prevState)
+                return {...prevState, [i]: prevState.colourSelected}})}/>)
+        }
 
     const paletteButtons=palette.map((colour)=> {
     return (<button key={colour} className="palette-selection"
-        style={{backgroundColor: `#${colour}`}}/>
+        style={{backgroundColor: `${colour}`}} onClick={()=> setEditorState(
+            (prevState) => ({...prevState, colourSelected: colour}
+            )            
+        )}/>
     )})
-
-    console.log(palette, paletteButtons)
 
     return (
         <div>
